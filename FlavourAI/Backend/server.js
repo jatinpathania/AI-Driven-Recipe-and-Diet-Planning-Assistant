c852dd1e8e3efa6645b8204a8a57663f0f7c1f2f
Jatin Pathania <jatinpathania12@outlook.com>
2025-11-07 17:43:53 +0530

    server.js file

--- FlavourAI/Backend/server.js
+++ FlavourAI/Backend/server.js
@@ -1,12 +1,20 @@
 const express = require("express")
 const dotenv= require('dotenv')
 const cors= require('cors')
+const PORT= process.env.PORT || 3000;
 const connectDB= require('./db/connectDB')
 const recipeRouters= require('./Routes/recipeRoutes.js')
-
+const app= express();
 dotenv.config();
+
+
 app.use(cors())
 app.use(express.json());
-const app= express();
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.listen(3000);
+connectDB();
+
+app.listen(PORT, ()=>{
+    console.log(`Server started at port ${PORT}`)
+});
