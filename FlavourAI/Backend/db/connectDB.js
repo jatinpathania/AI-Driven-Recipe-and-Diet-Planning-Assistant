c2cc548d8bb70b6589a8f89c58abbe587bd755f6
Jatin <jatinpathania1@gmail.com>
2025-11-07 17:43:15 +0530

    database connected successfully

--- FlavourAI/Backend/db/connectDB.js
+++ FlavourAI/Backend/db/connectDB.js
@@ -2,9 +2,6 @@ const mongoose= require('mongoose');
 
 const connectDB= async()=>{
     try{
-        if(!process.env.MONGODB_URI){
-            throw new Error('MONGODB_URI is not defined in the .env file');
-        }
         const conn= await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB connected ${conn.connection.host}`);
     }
