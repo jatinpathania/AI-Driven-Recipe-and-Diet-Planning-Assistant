9fc20c56855ffbd6828ca845917f6e5b702fae88
reallvaibhav <kumar1299.be23@chitkarauniversity.edu.in>
2026-05-22 18:19:31 +0530

    main layout

--- components/Layout/MainLayout.jsx
+++ components/Layout/MainLayout.jsx
@@ -3,8 +3,17 @@
 import React from 'react';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import { GuestUserProvider } from '@/context/GuestUserContext';
+import { usePathname } from 'next/navigation';
+import PremiumLayout from '@/components/Layout/PremiumLayout';
 
 const MainLayout = ({ children }) => {
+    const pathname = usePathname();
+    const isKitchen = pathname && pathname.startsWith('/kitchen');
+
+    if (isKitchen) {
+        return <PremiumLayout>{children}</PremiumLayout>;
+    }
+
     return (
         <GuestUserProvider>
             <div className="flex min-h-screen bg-[var(--cream)]">
